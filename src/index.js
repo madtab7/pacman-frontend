4c3af70a9fd3641d97eac247bc200e2ce697e9d1
madtab7 <madtab7@gmail.com>
2018-10-03 09:32:54 -0400

    refactored code a bit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-
-  // set up game board & corresponding elements
-    // array of arrays for additional levels, pass map[x] into renderWorld function
-  const map =[
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,8],
-  [1,1,1,1,2,2,2,2,2,1,2,2,2,2,2,1,1,1,1,8],
-  [1,1,1,1,2,1,1,1,2,1,2,1,1,1,2,1,1,1,1,8],
-  [1,1,1,1,2,1,2,2,2,2,2,2,2,1,2,1,1,1,1,8],
-  [1,1,1,1,2,2,2,1,1,5,1,1,2,2,2,1,1,1,1,8],
-  [1,1,1,1,2,1,2,2,2,2,2,2,2,1,2,1,1,1,1,8],
-  [1,1,1,1,2,1,1,2,2,1,2,2,1,1,2,1,1,1,1,8],
-  [1,1,1,1,2,2,2,2,2,1,2,2,2,2,2,1,1,1,1,8],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,8]
-]
-
-
   //render pacman world
   const world = document.getElementById("pacman-world")
 
@@ -50,37 +34,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
   // })
 
 ////////////////////////////////////////////////////////////////////////////////////
-
-  // 1 -- wall
-  // 2 -- coin
-  // 3 -- ground
-  // 5 -- pacman
-  // 8 -- linebreak
-
-  function renderWorld(){
-    world.innerHTML = ''; //empty from previous
-    var counter = 1;
-    for (let y=0; y< map.length; y++){
-      for (let x=0; x< map[y].length; x++){
-        if(map[y][x] === 1){
-          world.innerHTML += `<div id=${counter} class='wall'></div>`
-          counter +=1;
-        } else if(map[y][x] === 2){
-          world.innerHTML += `<div id=${counter} class='coin'></div>`
-          counter +=1;
-        } else if(map[y][x]===3){
-          world.innerHTML += `<div id=${counter} class='ground'></div>`
-          counter +=1;
-        } else if(map[y][x] === 5){
-          world.innerHTML += `<div id=${counter} class='pacman'></div>`
-          counter +=1;
-        } else if(map[y][x] === 8){
-          world.innerHTML += "<br>"
-        }
-      }
-    }
-  }
-  renderWorld();
 
 
   let newPacman = new Pacman(86,0)
@@ -118,9 +71,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
           counter.innerText = `${newPacman.coins}`
         }
         newPacman.position = parseInt(predictivePosition.id)
-        console.log(oldPosition)
-        console.log(predictivePosition)
-        console.log(newPacman)
         oldPosition.removeAttribute("class")
         predictivePosition.removeAttribute("class")
         oldPosition.className = 'ground'
@@ -255,20 +205,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   })
 
-
-//level up on click once level is complete
-document.addEventListener("click", (event)=>{
-  if (event.target.id === "levelup"){
-    debugger;
-  }
-})
-
-//replay same level on click after losing
-document.addEventListener("click", (event)=>{
-  if (event.target.id === "playagain"){
-    // renderWorld()
-  }
-})
 
 
 });
